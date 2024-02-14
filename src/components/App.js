8fdc1159ba47e3b48edf33d0274a937466e1c491
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2024-02-14 17:13:49 +0530

    feat: improve landing page content

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,16 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return <h1>Hello world!</h1>;
+    return (
+      <>
+        <h1>Hello world!</h1>
+        <h3>
+          This is a minimal react-boilerplate for build single page
+          applications.
+        </h3>
+        <h4>Tech Stack: Webpack5, Babel, React, React-dom</h4>
+      </>
+    );
   }
 }
 
